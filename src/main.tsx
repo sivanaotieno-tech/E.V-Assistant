6b4a1058241e58ea0cd94e24700e635d2c2aa4f4
sivanaotieno-tech <sivana.otieno@tatuinternational.novapioneer.com>
2026-08-16 19:15:17 +0300

    chore: remove obsolete first-run workaround import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BootGate } from './components/BootGate';
-import './enter-system-fix';
 import './styles/performance.css';
 
 createRoot(document.getElementById('root')!).render(
